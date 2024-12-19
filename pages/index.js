b901a2b078a30d4d9fb6faa72b292b122c0ec213
vishaldharam <vishal.dharam20@pccoepune.org>
2024-12-19 13:28:26 +0530

    certificate:added for testing abouv app

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,6 @@ export default function Home() {
         <meta property='og:image:type' content='image/png' />
         <meta property='og:image:width' content='300' />
         <meta property='og:image:height' content='300' />
-        <meta property='og:url' content={`http://localhost:3000/certificate/${certificateId}`} />
         <meta property='og:type' content='website' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
@@ -176,6 +175,36 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      {/* <section className="text-gray-600 body-font">
+  <div className="container px-5 py-2 mx-auto flex flex-wrap">
+ 
+    <div className="flex flex-wrap md:-m-2 -m-1">
+      <div className="flex flex-wrap w-1/2">
+        <div className="md:p-2 p-1 w-1/2">
+          <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/500x300"/>
+        </div>
+        <div className="md:p-2 p-1 w-1/2">
+          <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/501x301"/>
+        </div>
+        <div className="md:p-2 p-1 w-full">
+          <img alt="gallery" className="w-full h-full object-cover object-center block" src="https://dummyimage.com/600x360"/>
+        </div>
+      </div>
+      <div className="flex flex-wrap w-1/2">
+        <div className="md:p-2 p-1 w-full">
+          <img alt="gallery" className="w-full h-full object-cover object-center block" src="https://dummyimage.com/601x361"/>
+        </div>
+        <div className="md:p-2 p-1 w-1/2">
+          <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/502x302"/>
+        </div>
+        <div className="md:p-2 p-1 w-1/2">
+          <img alt="gallery" className="w-full object-cover h-full object-center block" src="https://dummyimage.com/503x303"/>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>  */}
 
       <section className='text-gray-600 mx-auto body-font overflow-hidden'>
         <div className='container   medium:px-2 mx-auto'>
