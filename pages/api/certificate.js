3b76bd9daf1ead06ac4425f6c0a5b8e62012b85a
vishaldharam <vishal.dharam20@pccoepune.org>
2024-12-21 15:43:24 +0530

    certificate:added

--- pages/api/certificate.js
+++ pages/api/certificate.js
@@ -1,56 +1,31 @@
-import puppeteer from 'puppeteer'
+import { ImageResponse } from '@vercel/og'
 
-export default async function handler(req, res) {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
+export const config = {
+  runtime: 'edge'
+}
 
-  // Example numerical data to be visualized
-  const data = {
-    0: 137,
-    1: 80,
-    2: 78,
-    3: 71,
-    4: 13,
-    5: 10,
-    6: 26,
-    7: 10,
-    8: 0,
-    9: 0,
-    10: 0
-  }
-
-  // Generate the HTML content for the data
-  const htmlContent = `
-    <html>
-      <body>
-        <div style="font-family: Arial; text-align: center; padding: 20px;">
-          <h1>Certificate</h1>
-          <p>This certifies that</p>
-          <h2>${req.query.name || 'John Doe'}</h2>
-          <p>has completed the course with Grade ${req.query.grade || 'A'}</p>
-          
-          <h3>Performance Data:</h3>
-          <table border="1" style="margin: 0 auto; text-align: center;">
-            <thead>
-              <tr>
-                <th>Index</th>
-                <th>Value</th>
-              </tr>
-            </thead>
-           
-          </table>
-        </div>
-      </body>
-    </html>
-  `
-
-  await page.setContent(htmlContent)
-  const screenshot = await page.screenshot({ type: 'png' })
-
-  console.log('screeenshit', screenshot)
-
-  await browser.close()
-
-  res.setHeader('Content-Type', 'image/png')
-  res.status(200).send(screenshot)
+export default async function handler() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          color: 'black',
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          padding: '50px 200px',
+          textAlign: 'center',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        👋 Hello 你好 नमस्ते こんにちは สวัสดีค่ะ 안녕 добрий день Hallá
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630
+    }
+  )
 }
